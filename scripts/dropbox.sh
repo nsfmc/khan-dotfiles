c8026909ceb329bddffd95042f3637364883c1de
Marcos Ojeda <subliminal@gmail.com>
2013-08-21 14:10:18 -0700

    install dropbox if not installed

---
+++ scripts/dropbox.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+source "./_helpers.sh"
+
+install_dropbox () {
+  notice "Installing dropbox"
+  if [[ -d "$HOME/Applications/Dropbox.app" || -d "/Applications/Dropbox.app" ]]
+  then
+    success "Found Dropbox app!"
+  else
+    info "Dropbox.app not found, installing\n"
+    brew cask install dropbox
+    success "Dropbox installed! Opening."
+    open "$HOME/Applications/Dropbox.app"
+    info "Go set up Dropbox! ${tty_bold}Press return when you finish.${tty_normal}"
+    read
+  fi
+}
+
+install_dropbox
